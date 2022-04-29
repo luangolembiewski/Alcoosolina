d53441b941e295896af780a998c205f8270e2b97
Luan Golembiewski <luangolembiewski74@gmail.com>
2022-04-29 19:25:13 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/alcoosolina/CalculoMedia.java
@@ -0,0 +1,4 @@
+package com.example.alcoosolina;
+
+public class CalculoMedia {
+}
---
+++ app/src/main/java/com/example/alcoosolina/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.alcoosolina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_inicial);
+    }
+
+    public void calculoMedia(View v){
+        setContentView(R.layout.calculo_media);
+    }
+
+
+    //public void postoProximo(View v){
+        //setContentView(R.layout.);
+    //}
+
+}
