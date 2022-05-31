2e332e38f1ce624abccdac9b6e674fa6f460ba4b
Luan Golembiewski <luangolembiewski74@gmail.com>
2022-05-31 17:30:40 -0300

    Loc em tempo real

--- app/src/main/java/com/example/alcoosolina/Mapa.java
+++ app/src/main/java/com/example/alcoosolina/Mapa.java
@@ -1,8 +1,20 @@
 package com.example.alcoosolina;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,31 +22,137 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.alcoosolina.databinding.TelaMapaBinding;
+import com.google.android.material.internal.ContextUtils;
 
-public class Mapa extends FragmentActivity implements OnMapReadyCallback {
+import java.util.List;
 
+public class Mapa extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, LocationListener {
+
+    private static final String TAG = "Mapa";
     private GoogleMap mMap;
+    private LocationManager locationManager;
+
+    EditText textDestino;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_mapa);
+        control();
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
+    public void control() {
+        textDestino = (EditText) findViewById(R.id.textDestino);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        //Solicita Permissões
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(Mapa.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(Mapa.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(Mapa.this, new String[]{Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+            return;
+        }
+
+        try {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 50000, 0, this);
+        } catch (SecurityException ex) {
+            Log.e(TAG, "ERROR", ex);
+        }
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.removeUpdates(this);
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //escolher melhor provedor
+            /*
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+            String provider = locationManager.getBestProvider(criteria, true);
+            Toast.makeText(getApplicationContext(), "Provider: "+ provider, Toast.LENGTH_LONG).show();
+             */
         mMap = googleMap;
+        mMap.setOnMapClickListener(this);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        // Add a marker in Sydney and move the camera
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+    }
 
-        LatLng sydney = new LatLng(-23, -52);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        googleMap.setTrafficEnabled(true);
+    @Override
+    public void onMapClick(@NonNull LatLng latLng) {
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Marcador"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        textDestino.setText(latLng.toString());
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),
+                location.getLongitude()),15.0f));
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull List<Location> locations) {
+        LocationListener.super.onLocationChanged(locations);
+        Toast.makeText(getApplicationContext(), "Posição Alterada", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onFlushComplete(int requestCode) {
+        LocationListener.super.onFlushComplete(requestCode);
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        LocationListener.super.onStatusChanged(provider, status, extras);
+        Toast.makeText(getApplicationContext(), "O Status do Provider foi alterado", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+        LocationListener.super.onProviderEnabled(provider);
+        Toast.makeText(getApplicationContext(), "Provider Habilitado", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+        LocationListener.super.onProviderDisabled(provider);
+        Toast.makeText(getApplicationContext(), "Provider Desabilitado", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
